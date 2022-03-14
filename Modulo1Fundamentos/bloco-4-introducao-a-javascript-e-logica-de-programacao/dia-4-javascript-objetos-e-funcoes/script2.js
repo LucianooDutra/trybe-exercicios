8637bf010c47c9286ef3511805e4a894fb534593
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-03-14 14:41:37 -0300

    exercicio bloco 5 aula 1

---
+++ Modulo1Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -0,0 +1,155 @@
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+// function verificarPalindromo(palindromo) {
+
+//     let invertido = '';
+//     let resultado = '';
+
+//     for (let index = palindromo.length - 1; index >= 0; index -= 1) {
+//         invertido += palindromo[index];
+//     }
+
+//     if (palindromo === invertido) {
+//         resultado = 'true';
+//     } else {
+//         resultado = 'false';
+//     }
+
+//     return resultado;
+
+// }
+
+// console.log(verificarPalindromo('arara'));
+
+
+
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+
+// function indiceMaiorValor(arr) {
+
+//     let resultado = '';
+//     let indice = '';
+
+//     for (let index = 0; index < arr.length; index += 1) {
+
+//         if (arr[index] > resultado) {
+//             resultado = arr[index];
+//             indice = index;
+//         }
+//     }
+
+//     return indice;
+
+// }
+
+// console.log(indiceMaiorValor([11, 3, 6, 7, 4, 20]));
+
+
+
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+
+// function indiceMenorValor(arr) {
+
+//     let resultado = '';
+//     let indice = '';
+
+//     for (let index = 0; index < arr.length; index += 1) {
+
+//         if (arr[index] < resultado) {
+//             resultado = arr[index];
+//             indice = index;
+//         }
+//     }
+
+//     return indice;
+
+// }
+
+// console.log(indiceMenorValor([11, 3, 6, 7, 1, -20]));
+
+
+
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+
+// function maiorNome(arr) {
+
+//     let resultado = '';
+//     let indice = '';
+
+//     for (let index = 0; index < arr.length; index += 1) {
+
+//         if (arr[index].length > resultado.length) {
+//             resultado = arr[index];
+//             indice = index;
+//         }
+//     }
+
+//     return resultado;
+
+// }
+
+// console.log(maiorNome(['Luciano', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+
+
+
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// function maiorRepetição(arr) {
+
+//     let resultado = '';
+//     let indice = '';
+
+//     for (let index = 0; index < arr.length; index += 1) {
+
+//         if (arr[index] > resultado) {
+//             resultado = arr[index];
+//             indice = index;
+//         }
+//     }
+
+//     return resultado;
+
+// }
+
+// console.log(maiorRepetição([2, 3, 2, 5, 8, 2, 3]));
+
+
+
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+
+
+// function somatorio(numero) {
+
+
+//     let resultado = 0;
+//     let indice = '';
+
+//     for (let index = 1; index <= numero; index += 1) {
+
+//             resultado =+ resultado + [index];
+//             indice = index;
+        
+//     }
+
+//     return resultado;
+
+// }
+
+// console.log(somatorio([5]));
+
+
+
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+
+
