2aa11aeee78f983e0bc52b9b9ecbc7119299a3d3
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-04-04 18:47:07 -0300

    Parte 2, exercicio 2

--- Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -34,25 +34,35 @@ const order = {
   },
 };
 
-
+// Exercicio 1
 // "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
 
 const customerInfo = () => {
-//   Adicione abaixo as informações necessárias.
+  //   Adicione abaixo as informações necessárias.
+
   const ana = order.order.delivery.deliveryPerson;
   const rafael = order.name;
   const numeroTel = order.phoneNumber;
   const rua = order.address.street;
   const numeroCasa = order.address.number;
   const numeroApt = order.address.apartment;
-  
-  console.log(`Olá ${ana}, entrega para: ${rafael}, Telefone: ${numeroTel}, R. ${rua}, N°: ${numeroCasa}, AP: ${numeroApt}`);
+
+  console.log(
+    `Olá ${ana}, entrega para: ${rafael}, Telefone: ${numeroTel}, R. ${rua}, N°: ${numeroCasa}, AP: ${numeroApt}`
+  );
 };
 
-customerInfo();
+// customerInfo();
 
-const orderModifier = (order) => {
-//   // Adicione abaixo as informações necessárias.
- };
+const orderModifier = () => {
+  // Adicione abaixo as informações necessárias.
 
-// orderModifier(order);
+  order.name = "Luiz Silva";
+  order.payment.total = "50,00";
+
+  console.log(
+    `Olá ${order.name}, o total do seu pedido de marguerita, pepperoni e ${order.order.drinks.coke.type} é R$ ${order.payment.total}`
+  );
+};
+
+// orderModifier();
