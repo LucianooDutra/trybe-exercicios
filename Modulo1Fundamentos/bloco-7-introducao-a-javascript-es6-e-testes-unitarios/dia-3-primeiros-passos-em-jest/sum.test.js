b9b58fa27e899e6cbb640be14ba5d14394eb6ae0
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-04-05 20:35:07 -0300

    Item 3 adicionado

--- Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -30,3 +30,21 @@ describe("Item 2", () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
+
+describe("Item 3", () => {
+  test("Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado", () => {
+    expect(myFizzBuzz(15)).toBe("fizzbuzz");
+  });
+  test("Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado", () => {
+    expect(myFizzBuzz(9)).toBe("fizz");
+  });
+  test("Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado", () => {
+    expect(myFizzBuzz(20)).toBe("buzz");
+  });
+  test("Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado", () => {
+    expect(myFizzBuzz(4)).toBe(4);
+  });
+  test("Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado", () => {
+    expect(myFizzBuzz("4")).toBe(false);
+  });
+});
