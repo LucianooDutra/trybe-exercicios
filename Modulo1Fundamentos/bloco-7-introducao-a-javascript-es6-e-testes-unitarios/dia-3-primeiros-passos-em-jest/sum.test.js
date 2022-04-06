29a6bd1aa9d565d22dc4ea9a559fc1c9612ced54
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-04-05 21:03:00 -0300

    Item 4 adicionado

--- Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Modulo1Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,6 +1,7 @@
 const { describe, it, expect } = require("@jest/globals");
-const { sum, myRemove, myFizzBuzz } = require("./sum.js");
+const { sum, myRemove, myFizzBuzz, encode, decode } = require("./sum.js");
 
+// Item 1
 describe("Item 1", () => {
   test("Teste se o retorno de sum(4, 5) é 9", () => {
     expect(sum(4, 5)).toBe(9);
@@ -19,6 +20,7 @@ describe("Item 1", () => {
   });
 });
 
+// Item 2
 describe("Item 2", () => {
   test("Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado", () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
@@ -31,6 +33,7 @@ describe("Item 2", () => {
   });
 });
 
+// Item 3
 describe("Item 3", () => {
   test("Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado", () => {
     expect(myFizzBuzz(15)).toBe("fizzbuzz");
@@ -46,5 +49,27 @@ describe("Item 3", () => {
   });
   test("Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado", () => {
     expect(myFizzBuzz("4")).toBe(false);
+  });
+});
+
+// Item 4
+describe("Item 4", () => {
+  test("Teste se encode e decode são funções", () => {
+    expect(typeof encode).toBe("function");
+  });
+  test("Teste se encode e decode são funções", () => {
+    expect(typeof decode).toBe("function");
+  });
+  test("Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente", () => {
+    expect(encode("ontem fui a praia")).toBe("4nt2m f53 1 pr131");
+  });
+  test("Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u , respectivamente", () => {
+    expect(decode("4nt2m f53 1 pr131")).toBe("ontem fui a praia");
+  });
+  test("Teste se as demais letras/números não são convertidos para cada caso", () => {
+    expect(encode("ontem fui a praia")).toBe("4nt2m f53 1 pr131");
+  });
+  test("Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro", () => {
+    expect(encode("ontem fui a praia").length).toBe(17);
   });
 });
